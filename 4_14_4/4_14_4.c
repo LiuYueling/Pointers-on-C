35369941fac63ee4fdeea174e2ad535593a41e43
yueling_liu <yueling_liu@outlook.com>
2019-03-29 14:18:46 +0800

    add 4_14_4练习

--- 4_14_4/4_14_4.c
+++ 4_14_4/4_14_4.c
@@ -1,10 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**********************************************************************
+- Function :	void copy_n(char dst[], char src[], int len)
+- Description :	将src数组复制到dst数组中
+**********************************************************************/
+void copy_n(char dst[], char src[], int len)
+{
+	int i;
+	
+	for(i=0; len>i; ++i)
+	{
+		*(dst + i) = *(src + i);
+	}
+}
+
 int main()
 {
+	char a[] = "hello world!";
+	char b[sizeof(a)/sizeof(char)];
 	
+	copy_n(b, a, sizeof(a)/sizeof(char));
 	
+	printf("%s\n", b);
 	system("pause");
 	
 	return 0;
