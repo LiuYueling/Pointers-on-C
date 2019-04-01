b100a2aaa1db740bd61f2f5a78e8f9e8d562a02f
yueling_liu <yueling_liu@outlook.com>
2019-04-01 18:10:14 +0800

    add 4_14_5

---
+++ 4_14_5/4_14_5.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char **Str_List;
+
+/*从按键获取字符串表*/
+void Scanf_String_List(void)
+{
+	unsigned int i = 0;
+	char Str_Scanf_BUFF[254];
+	char *Str_List_BUFF[254]; 
+	
+	printf("输入的字符串行数(最多%u行)\n",254);
+	printf("请输入字符串行，最多%u字符，最后一行以\"#\"结束\n", 128);
+	
+	do{
+		// scanf("%s", Str_Scanf_BUFF);
+		gets(Str_Scanf_BUFF);//输入字符串
+		*(Str_List_BUFF + i) = 
+			(char *)malloc((strlen(Str_Scanf_BUFF) + 1) * sizeof(char));//+1'\0'
+		strcpy(*(Str_List_BUFF + i), Str_Scanf_BUFF);//复制字符串
+		++i;
+	}while(*Str_Scanf_BUFF != '#');
+	
+	free(*(Str_List_BUFF + i - 1));
+	*(Str_List_BUFF + i - 1) = NULL;
+	
+	Str_List = (char **)malloc(sizeof(char*) * i);
+	memcpy(Str_List, Str_List_BUFF, sizeof(char**)*i);//
+	
+	printf("******************************分界线******************************\n");
+}
+
+/*释放内存*/
+void free_String_List(void)
+{
+	int i;
+	
+	for(i=0; *(Str_List + i) != 0; i++)
+	{
+		free(*(Str_List + i));
+		*(Str_List + i) = NULL;
+	}
+	free(Str_List);
+	Str_List = NULL;
+}
+
+
+int main()
+{
+	int list_len,i,j;
+	
+	Scanf_String_List();
+	
+	for(i=0; *(Str_List + i) != 0; i++)
+	{
+		// printf("%s", *(Str_List + i));
+		puts(*(Str_List + i));//会自动加\n
+	}
+	printf("******************************分界线******************************\n");
+	
+	
+	for(list_len=1; *(Str_List + list_len) != 0; list_len++);
+	{
+			
+		// strcmmp(*(Str_List + list_len - 1), *(Str_List + list_len))
+	}
+	system("pause");
+	free_String_List();
+	
+	
+	return 0;
+}
+
